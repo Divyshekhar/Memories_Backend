a595fafeeab863ccc93caa3a5431c62e95083d2b
Divyshekhar Sinha <divyshekharsinha@Divyshekhars-MacBook-Air.local>
2025-01-10 20:52:20 +0530

    adding functionalities

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js'
+import dotenv from 'dotenv';
+ 
 const app = express();
+dotenv.config();
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
@@ -11,9 +14,8 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://divyshekhardb:admin@cluster0.qougfag.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-const PORT = process.env.PORT || 3000;
+ const PORT = process.env.PORT || 3000;
 
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server running on: ${PORT}`)))
     .catch((err) => console.log(err.message));
